6d5d94854c8af4ee7906707245c1b5be6fd9e560
johndoe2019 <54654763+balejames@users.noreply.github.com>
2019-11-29 10:23:01 -0800

    Add bootstrap

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>Header</h1>
+    <div>
+        <b-nav>
+            <b-nav-item active>Fancy Quiz App</b-nav-item>
+            <b-nav-item>Counter: 4/10</b-nav-item>
+        </b-nav>
+    </div>
 </template>
